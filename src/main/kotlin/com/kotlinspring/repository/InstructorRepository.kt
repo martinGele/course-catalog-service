02bca5f4fbd55dd6238e6cf3642f40689c93a0f9
martin.gelevski <mgelevski-ext@visionbank.com.sa>
2024-07-05 15:25:33 +0200

    Adding tests for InstructorControllerIntgTest.kt

--- src/main/kotlin/com/kotlinspring/repository/InstructorRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/InstructorRepository.kt
@@ -3,5 +3,4 @@ package com.kotlinspring.repository
 import com.kotlinspring.entity.Instructor
 import org.springframework.data.repository.CrudRepository
 
-interface InstructorRepository : CrudRepository<Instructor,Int> {
-}
+interface InstructorRepository : CrudRepository<Instructor,Int>
--- src/test/intg/com/kotlinspring/controller/InstructorControllerIntgTest.kt
+++ src/test/intg/com/kotlinspring/controller/InstructorControllerIntgTest.kt
@@ -1,4 +1,66 @@
 package com.kotlinspring.controller
 
+import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.dto.InstructorDTO
+import com.kotlinspring.repository.CourseRepository
+import com.kotlinspring.repository.InstructorRepository
+import com.kotlinspring.util.courseEntityList
+import com.kotlinspring.util.instructorEntity
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
 class InstructorControllerIntgTest {
+
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+
+    @Autowired
+    lateinit var instructorRepository: InstructorRepository
+
+
+    @BeforeEach
+    fun setUp() {
+
+        instructorRepository.deleteAll()
+        val instructor = instructorEntity()
+        instructorRepository.save(instructor)
+
+    }
+
+
+    @Test
+    fun addCourse() {
+
+        val instructor = instructorRepository.findAll().first()
+
+
+        val instructorDTO = InstructorDTO(null, "Martin Gelevski")
+
+        val savedInstructorDTO = webTestClient
+            .post()
+            .uri("/v1/instructors")
+            .bodyValue(instructorDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(InstructorDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedInstructorDTO!!.id != null
+        }
+    }
+
 }
